d17b23f95d5a83d4c35eb2fffcc9780cb764c7d4
Jim Fowler <fowler@math.osu.edu>
2022-01-03 13:56:37 -0500

    softer language

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
       awsProfile: 'default',
       region: 'us-east-2',
       bucket: 'recommend.rossprogram.org',
-      createBucket: false,
+      createBucket: true,
       staticHosting: true,
       staticIndexPage: 'index.html',
       staticErrorPage: 'index.html',
